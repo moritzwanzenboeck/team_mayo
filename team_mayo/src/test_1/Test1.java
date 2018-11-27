0339e007d1d4fab5284eed033bc6ccafccc59f1b
Moritz <Moritz@DESKTOP-EMM01I8.fritz.box>
2018-11-27 21:24:04 +0100

    MXW

--- team_mayo/src/test_1/Test1.java
+++ team_mayo/src/test_1/Test1.java
@@ -1,9 +1,14 @@
 package test_1;
 
 
+import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.SensorModes;
 import lejos.robotics.RegulatedMotor;
 import lejos.utility.Delay;
 
@@ -11,6 +16,11 @@ public class Test1 {
 	public static void main(String args[]) {
 	      RegulatedMotor motorB = new EV3MediumRegulatedMotor(MotorPort.B);
 	      RegulatedMotor motorC = new EV3MediumRegulatedMotor(MotorPort.C);
+	      Port port = LocalEV3.get().getPort("S1");
+	      SensorModes sensor = new EV3TouchSensor(port);
+	      
+	      
+	      	      
 	      
 	      motorB.setSpeed(250);
 	      motorC.setSpeed(250);
@@ -18,8 +28,18 @@ public class Test1 {
 	      motorC.backward();
 	      Delay.msDelay(5000);
 	      motorB.stop();
-	      motorB.close();
 	      motorC.stop();
+	      
+	      if(sensor.getAvailableModes()) {
+	    	  motorB.forward();
+		      motorC.backward();
+		      Delay.msDelay(5000);
+		      motorB.stop();
+		      motorC.stop();
+	      };
+	      
+	      
+	      motorB.close();
 	      motorC.close();
 	      System.out.println("Test!icle");
 	      
